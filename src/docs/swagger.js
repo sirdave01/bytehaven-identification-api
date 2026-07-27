e039b724b93d114fcbb8043b2471608191f05d67
sirdave01 <d08178084956@gmail.com>
2026-07-27 08:19:35 -0600

    fixed

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -5,7 +5,9 @@ import { roleSchema } from "./schemas/roleSchema.js";
 import { applicationSchema } from "./schemas/applicationSchema.js";
 import { systemSettingSchema } from "./schemas/systemSettingSchema.js";
 
+
 const isProduction = process.env.NODE_ENV === "production";
+
 
 const doc = {
 
@@ -16,21 +18,29 @@ const doc = {
         version: "1.0.0"
     },
 
-    host: process.env.SWAGGER_HOST || "localhost:3000",
 
-    basePath: "/",
+    host: isProduction
+        ? "bytehaven-identification-api.onrender.com"
+        : "localhost:3000",
 
-    schemes: [
-        process.env.SWAGGER_SCHEME || "http"
-    ],
+
+    basePath: "/api/v1",
+
+
+    schemes: isProduction
+        ? ["https"]
+        : ["http"],
+
 
     consumes: [
         "application/json"
     ],
 
+
     produces: [
         "application/json"
     ],
+
 
     definitions: {
         ...userSchema,
@@ -38,12 +48,16 @@ const doc = {
         ...applicationSchema,
         ...systemSettingSchema
     }
+
 };
 
+
 const outputFile = "./src/docs/swagger-output.json";
+
 
 const endpointsFiles = [
     "./server.js"
 ];
+
 
 swaggerAutogen()(outputFile, endpointsFiles, doc);
